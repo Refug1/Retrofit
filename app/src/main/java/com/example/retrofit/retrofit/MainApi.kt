786bf3c0af70c1d885605a79b2fe7f14592ccacd
Refug1 <kirillezic@gmail.com>
2023-06-10 11:47:53 +0300

    Lession 8

--- app/src/main/java/com/example/retrofit/retrofit/MainApi.kt
+++ app/src/main/java/com/example/retrofit/retrofit/MainApi.kt
@@ -1,4 +1,5 @@
 package com.example.retrofit.retrofit
+
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -6,22 +7,23 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.Response
+
 
 interface MainApi {
-    @GET("products/{id}")
+    @GET("auth/products/{id}")
     suspend fun getProductById(@Path("id") id: Int): Product
 
     @POST("auth/login")
-    suspend fun auth(@Body authRequest: AuthRequest): User
+    suspend fun auth(@Body authRequest: AuthRequest): Response<User>
 
-    @GET("products")
-    suspend fun getAllProducts(): Products
-
-    @GET("products/search")
-    suspend fun getProductsByName(@Query("q") name: String): Products
+    @Headers("Content-Type: application/json")
+    @GET("auth/products")
+    suspend fun getAllProducts(@Header("Authorization") token: String): Products
 
     @Headers("Content-Type: application/json")
     @GET("auth/products/search")
     suspend fun getProductsByNameAuth(@Header("Authorization") token: String,
                                       @Query("q") name: String): Products
+
 }
